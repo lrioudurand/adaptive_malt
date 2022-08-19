49bf3a981dd83618704ca09213eae3a08c70ac87
Lionel Riou-Durand <lionel.riou-durand@warwick.ac.uk>
2022-08-19 20:07:18 +0100

    implementation of adaptive C_alpha criterion + mini_batch

--- R/mini_batch.R
+++ R/mini_batch.R
@@ -18,14 +18,15 @@ eta_h=0.05
 eta_w=3
 kappa=8
 eta_noise=0.001
+batch_size=2
 
 alpha_adam=0.005
-beta1_adam=0
+beta1_adam=0.9
 beta2_adam=0.999
 eps_adam=10^(-8)
 
 
-osam=function(U,grad,pos,tau,h,w,m,m_sq,v_sq,cov_sq,deriv_sq,m_adam,v_adam,iter){
+osam=function(U,grad,pos,tau,h,w,m,m_sq,v_sq,cov_sq,deriv_sq,m_adam,v_adam,iter,noisy_grad){
   eigen_max=sqrt(sum(w^2))
   eigen_vector=w/eigen_max
   if(any(is.na(eigen_vector))){eigen_vector=w*0}
@@ -52,85 +53,92 @@ osam=function(U,grad,pos,tau,h,w,m,m_sq,v_sq,cov_sq,deriv_sq,m_adam,v_adam,iter)
   U_x=U(x);U_malt_x=U_x
   norm_draws=array(stats::rnorm(d*(L+1)),dim=c(d,L+1))
   expo_draw=stats::rexp(1)
- # malt update
-    v=norm_draws[,steps]
-    Delta=0
-    v_0=v
-    for(j in 1:L){
-      v=eta*v+zeta*norm_draws[,j]
-      v=v-half*grad_x
-      x=x+h*v
-      grad_y=grad(x)
-      Delta=Delta-half*sum(v*(grad_x+grad_y))
-      v=v-half*grad_y
-      grad_x=grad_y
-    }
-    prop=x
-    gradsq_x=sum(grad_x^2)
-    U_x=U(x)
-    Delta=small*(gradsq_x-gradsq_malt_x)+U_x-U_malt_x+Delta
-    if(expo_draw<Delta){x=pos;grad_x=grad_malt_x;gradsq_x=gradsq_malt_x;U_x=U_malt_x}else{grad_malt_x=grad_x;gradsq_malt_x=gradsq_x;U_malt_x=U_x}
-    #update m
-    eta_m=1/(ceiling(i/kappa)+1)
-    m=(1-eta_m)*m+eta_m*x
-    #update w
-    x_c=x-m
-    if(i>eta_w & eigen_max>0){
+  # malt update
+  v=norm_draws[,steps]
+  Delta=0
+  v_0=v
+  for(j in 1:L){
+    v=eta*v+zeta*norm_draws[,j]
+    v=v-half*grad_x
+    x=x+h*v
+    grad_y=grad(x)
+    Delta=Delta-half*sum(v*(grad_x+grad_y))
+    v=v-half*grad_y
+    grad_x=grad_y
+  }
+  prop=x
+  gradsq_x=sum(grad_x^2)
+  U_x=U(x)
+  Delta=small*(gradsq_x-gradsq_malt_x)+U_x-U_malt_x+Delta
+  if(expo_draw<Delta){x=pos;grad_x=grad_malt_x;gradsq_x=gradsq_malt_x;U_x=U_malt_x}else{grad_malt_x=grad_x;gradsq_malt_x=gradsq_x;U_malt_x=U_x}
+  #update m
+  eta_m=1/(ceiling(i/kappa)+1)
+  m=(1-eta_m)*m+eta_m*x
+  #update w
+  x_c=x-m
+  if(i>eta_w & eigen_max>0){
     w=w*(i-eta_w)/(i+1)+x_c*(sum(x_c*eigen_vector))*(eta_w+1)/(i+1)
-    }else{w=x_c}
-    #update tau
-    pos_c=sum((pos-m)*eigen_vector)
-    prop_c=sum((prop-m)*eigen_vector)
-    v_c=sum(v*eigen_vector)
-    v_0=sum(v_0*eigen_vector)
-    diff_sq=((prop_c)^2-(pos_c)^2)
-    sum_sq=((prop_c)^2+(pos_c)^2)
-    #update m_sq, v_sq and rho
-    m_sq=(1-eta_m)*m_sq+eta_m*(pos_c^2+prop_c^2)/2
-    if(i>eta_w & eigen_max>0){
+  }else{w=x_c}
+  #update tau
+  pos_c=sum((pos-m)*eigen_vector)
+  prop_c=sum((prop-m)*eigen_vector)
+  v_c=sum(v*eigen_vector)
+  v_0=sum(v_0*eigen_vector)
+  diff_sq=((prop_c)^2-(pos_c)^2)
+  sum_sq=((prop_c)^2+(pos_c)^2)
+  #update m_sq, v_sq and rho
+  m_sq=(1-eta_m)*m_sq+eta_m*(pos_c^2+prop_c^2)/2
+  if(i>eta_w & eigen_max>0){
     v_sq=v_sq*(i-eta_w)/(i+1)+0.5*((pos_c^2-m_sq)^2+(pos_c^2-m_sq)^2)*(eta_w+1)/(i+1)
     cov_sq= cov_sq*(i-eta_w)/(i+1)+(pos_c^2-m_sq)*(prop_c^2-m_sq)*(eta_w+1)/(i+1)
     deriv_sq=deriv_sq*(i-eta_w)/(i+1)+diff_sq*(prop_c*v_c+pos_c*v_0)*(eta_w+1)/(i+1)
     rho=cov_sq/v_sq
     rho_prime=deriv_sq/v_sq
     #noisy_grad=rho_prime-((1-min(1,rho)^2))/(2*h*L)
-    noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(0.5*(1+min(1,rho))/(h*L))*(diff_sq)^2
+    if(i%%batch_size!=0){
+    noisy_grad=noisy_grad+2*diff_sq*(prop_c*v_c+pos_c*v_0)-(0.5*(1+min(1,rho))/(h*L))*(diff_sq)^2
+    }else{noisy_grad=noisy_grad/batch_size}
     #if(i>20000){noisy_grad=2*v_sq*(rho_prime-((1-min(1,rho)^2))/(2*h*L))}
     #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(v_sq*(1-min(1,rho)^2)/(h*L))
-    }else{noisy_grad=0}
-    #noisy_grad=4*diff_sq*prop_c*v_c-(1/tau)*(diff_sq)^2
+  }else{noisy_grad=0}
+  #noisy_grad=4*diff_sq*prop_c*v_c-(1/tau)*(diff_sq)^2
   #noisy_grad=4*diff_sq*(pos_c*v_0)-(1/tau)*(diff_sq)^2
-    #=4*diff_sq*prop_c*v_c-(0.5/tau)*(diff_sq)^2
-    #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(0.5*(1+min(1,rho))/(h*L))*(diff_sq)^2
+  #=4*diff_sq*prop_c*v_c-(0.5/tau)*(diff_sq)^2
+  #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(0.5*(1+min(1,rho))/(h*L))*(diff_sq)^2
 
-    #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(v_sq/(h*L))*(1-min(1,rho)^2)
-    #adam
-     m_adam=beta1_adam*m_adam+(1-beta1_adam)*noisy_grad
-     v_adam=beta2_adam*v_adam+(1-beta2_adam)*noisy_grad^2
-     m_hat=m_adam/(1-beta1_adam^i)
-     v_hat=v_adam/(1-beta2_adam^i)
-     #tau=tau+alpha_adam*m_hat/(sqrt(v_hat)+eps_adam)
-     #current_alpha=alpha_adam*5^(1-i/n)
-     current_alpha=alpha_adam
-    log_tau=log(tau)+current_alpha*m_hat/(sqrt(v_hat)+eps_adam)
-    #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0+sign(prop_c*v_c*pos_c*v_0)*sqrt(abs(prop_c*v_c*pos_c*v_0)))-(1/tau)*(diff_sq)^2
-    #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(2/tau)*(m_sq-(pos_c*prop_c)^2)
-    #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(0.75/tau)*(diff_sq)^2
-    #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(1/tau)*(diff_sq)^2-(0.5/tau^2)
-    #noisy_grad=-4*(pos_c)^2*prop_c*v_c-(1/tau)*(diff_sq)^2
-    #noisy_grad=-4*((pos_c)^2-eigen_max)*prop_c*v_c-(1/tau)*(diff_sq)^2
-    #noisy_grad=4*diff_sq*prop_c*v_c*(1/tau)-(1/tau^2)*(diff_sq)^2
-    #log_tau=log(tau)+eta_noise*(noisy_grad)
-    #log_tau=log(tau)+eta_noise*exp(-2*i/n)*(noisy_grad)
-    #log_tau=log(tau)+eta_noise*(noisy_grad)
-    tau=exp(log_tau)
-    #tau=tau+eta_noise*(noisy_grad)
-    #update h
-    h=0.05
+  #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(v_sq/(h*L))*(1-min(1,rho)^2)
+  #adam
+  if(i%%batch_size==0){
+  m_adam=beta1_adam*m_adam+(1-beta1_adam)*noisy_grad
+  v_adam=beta2_adam*v_adam+(1-beta2_adam)*noisy_grad^2
+  m_hat=m_adam/(1-beta1_adam^i)
+  v_hat=v_adam/(1-beta2_adam^i)
+  current_alpha=alpha_adam
+  log_tau=log(tau)+current_alpha*m_hat/(sqrt(v_hat)+eps_adam)
+  tau=exp(log_tau)
+  noisy_grad=0
+  }
+  #tau=tau+alpha_adam*m_hat/(sqrt(v_hat)+eps_adam)
+  #current_alpha=alpha_adam*5^(1-i/n)
+
+  #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0+sign(prop_c*v_c*pos_c*v_0)*sqrt(abs(prop_c*v_c*pos_c*v_0)))-(1/tau)*(diff_sq)^2
+  #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(2/tau)*(m_sq-(pos_c*prop_c)^2)
+  #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(0.75/tau)*(diff_sq)^2
+  #noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(1/tau)*(diff_sq)^2-(0.5/tau^2)
+  #noisy_grad=-4*(pos_c)^2*prop_c*v_c-(1/tau)*(diff_sq)^2
+  #noisy_grad=-4*((pos_c)^2-eigen_max)*prop_c*v_c-(1/tau)*(diff_sq)^2
+  #noisy_grad=4*diff_sq*prop_c*v_c*(1/tau)-(1/tau^2)*(diff_sq)^2
+  #log_tau=log(tau)+eta_noise*(noisy_grad)
+  #log_tau=log(tau)+eta_noise*exp(-2*i/n)*(noisy_grad)
+  #log_tau=log(tau)+eta_noise*(noisy_grad)
+
+  #tau=tau+eta_noise*(noisy_grad)
+  #update h
+  h=0.05
 
 
-    #storing noisy_grad
-    return(list(pos=x,tau=tau,h=h,w=w,m=m,m_sq=m_sq,v_sq=v_sq,cov_sq=cov_sq,deriv_sq=deriv_sq,m_adam=m_adam,v_adam=v_adam,iter=i+1,noisy_grad=noisy_grad))
+  #storing noisy_grad
+  return(list(pos=x,tau=tau,h=h,w=w,m=m,m_sq=m_sq,v_sq=v_sq,cov_sq=cov_sq,deriv_sq=deriv_sq,m_adam=m_adam,v_adam=v_adam,iter=i+1,noisy_grad=noisy_grad))
 }
 
 
@@ -147,7 +155,7 @@ n=10000
 
 
 adaptive_malt=function(n,init){
-  update=list(pos=init,tau=1.8,h=0.2,w=init,m=init,m_sq=sum(init^2),v_sq=0,cov_sq=0,deriv_sq=0,m_adam=0,v_adam=0,iter=1)
+  update=list(pos=init,tau=1.8,h=0.2,w=init,m=init,m_sq=sum(init^2),v_sq=0,cov_sq=0,deriv_sq=0,m_adam=0,v_adam=0,iter=1,noisy_grad=0)
   eig_values=rep(NA,n)
   means=rep(NA,n)
   tau_values=rep(NA,n)
@@ -156,7 +164,7 @@ adaptive_malt=function(n,init){
   rho_values=rep(NA,n)
   rho_prime_values=rep(NA,n)
   for(i in 1:n){
-    update=osam(U,grad,update$pos,update$tau,update$h,update$w,update$m,update$m_sq,update$v_sq,update$cov_sq,update$deriv_sq,update$m_adam,update$v_adam,update$iter)
+    update=osam(U,grad,update$pos,update$tau,update$h,update$w,update$m,update$m_sq,update$v_sq,update$cov_sq,update$deriv_sq,update$m_adam,update$v_adam,update$iter,update$noisy_grad)
     eig_values[i]=sqrt(sum(update$w^2))
     means[i]=sum(update$w*update$m)/sqrt(sum(update$w^2))
     tau_values[i]=update$tau
@@ -186,24 +194,16 @@ plot(output$tau[1:150],type="l")
 plot(output$tau[1:1000],type="l")
 plot(output$tau,type="l")
 
-# output$last_eigen_vector
-# mean(output$tau[2000:n])
-# var(output$tau[2000:n])
-# var(output$noisyg[2000:n])
+
 
 tau_bar=0
 tau_average=rep(NA,n)
 for(i in 1:n){
-    tau_bar=beta2_adam*tau_bar+(1-beta2_adam)*output$tau[i]
-    tau_average[i]=tau_bar/(1-beta2_adam^i)
+  tau_bar=beta2_adam*tau_bar+(1-beta2_adam)*output$tau[i]
+  tau_average[i]=tau_bar/(1-beta2_adam^i)
 }
 plot(tau_average,type="l")
 var(output$noisyg)
+var(output$noisyg[(1:n/batch_size)*batch_size])
 var(output$tau)
 tau_average[n]
-
-#plot(tau_average[1:5000],type="l")
-#tau_average[5000]
-
-#plot(output$rho_prime_values,type="l")
-#plot(output$rho_prime_values[200:n],type="l")
--- R/pseudo_code.R
+++ R/pseudo_code.R
@@ -23,6 +23,7 @@ eta_h=0.05
 eta_w=3
 kappa=8
 
+
 # hyperparameters for dual averaging of step size
 gamma = 0.05
 t0 = 10
@@ -45,11 +46,13 @@ kappa_stepsize = 0.75
 # }
 
 
-osam=function(U,grad,pos,tau,h,w,m,iter,
+osam=function(U,grad,pos,tau,h,w,m,m_adam,v_adam,iter,
               H_bar, h_bar, mu, n_warmup, delta){
   eigen_max=sqrt(sum(w^2))
   eigen_vector=w/eigen_max
-  g=2/sqrt(eigen_max) #look at the meads paper to improve
+  if(any(is.na(eigen_vector))){eigen_vector=w*0}
+  g=1/sqrt(eigen_max)
+  #g=2/tau
   eta=exp(-g*h)
   zeta=sqrt(1-eta^2)
   half=h/2
@@ -58,7 +61,9 @@ osam=function(U,grad,pos,tau,h,w,m,iter,
   if(i<100){
     tau=h
   }
-  L=ceiling(tau/h)
+  if(i==100){tau=1.8*eigen_max}
+  if(tau<h){tau=h}
+  L=floor(tau/h)
   steps=L+1
   #malt_x=pos
   x=pos
@@ -69,21 +74,24 @@ osam=function(U,grad,pos,tau,h,w,m,iter,
   expo_draw=stats::rexp(1)
   # malt update
   v=norm_draws[,steps]
+  v_0=eta*v+zeta*norm_draws[,1]
   Delta=0
   for(j in 1:L){
+    v=eta*v+zeta*norm_draws[,j]
     v=v-half*grad_x
     x=x+h*v
     grad_y=grad(x)
     Delta=Delta-half*sum(v*(grad_x+grad_y))
     v=v-half*grad_y
-    v=eta*v+zeta*norm_draws[,j]
     grad_x=grad_y
   }
   prop=x
   gradsq_x=sum(grad_x^2)
   U_x=U(x)
   Delta=small*(gradsq_x-gradsq_malt_x)+U_x-U_malt_x+Delta
-  if(expo_draw<Delta){x=pos;grad_x=grad_malt_x;gradsq_x=gradsq_malt_x;U_x=U_malt_x}else{grad_malt_x=grad_x;gradsq_malt_x=gradsq_x;U_malt_x=U_x}
+  if(expo_draw<Delta){
+    x=pos;grad_x=grad_malt_x;gradsq_x=gradsq_malt_x;U_x=U_malt_x
+    }else{grad_malt_x=grad_x;gradsq_malt_x=gradsq_x;U_malt_x=U_x}
 
   if (i <= n_warmup) {
     #update m
@@ -91,19 +99,26 @@ osam=function(U,grad,pos,tau,h,w,m,iter,
     m=(1-eta_m)*m+eta_m*x
 
     #update w
-    x_c=x-m
-    if(i>eta_w){
+    if(i>eta_w & eigen_max>0){
+      x_c=x-m
       w=w*(i-eta_w)/(i+1)+x_c*(sum(x_c*eigen_vector))*(eta_w+1)/(i+1)
-    }else{w=x_c}
+    }
 
     #update tau
+    if(i>eta_w & eigen_max>0){
     pos_c=sum((pos-m)*eigen_vector)
     prop_c=sum((prop-m)*eigen_vector)
     v_c=sum(v*eigen_vector)
+    v_0=sum(v_0*eigen_vector)
     diff_sq=((prop_c)^2-(pos_c)^2)
-    noisy_grad=4*diff_sq*prop_c*v_c-(1/tau)*(diff_sq)^2
-    tau=1.8  # Q: does this mean tau does not get updated?
-
+    noisy_grad=2*diff_sq*(prop_c*v_c+pos_c*v_0)-(0.75/(h*L))*(diff_sq)^2
+    m_adam=beta1_adam*m_adam+(1-beta1_adam)*noisy_grad
+    v_adam=beta2_adam*v_adam+(1-beta2_adam)*noisy_grad^2
+    m_hat=m_adam/(1-beta1_adam^i)
+    v_hat=v_adam/(1-beta2_adam^i)
+    log_tau=log(tau)+alpha_adam*m_hat/(sqrt(v_hat)+eps_adam)
+    tau=exp(log_tau)
+    }else{noisy_grad=0}
     #update h
     H_bar = (1 - 1 / (i + t0)) * H_bar + (delta - min(1, exp(-Delta))) / (i + t0)
     log_h = mu - sqrt(i) / gamma * H_bar
@@ -111,18 +126,24 @@ osam=function(U,grad,pos,tau,h,w,m,iter,
     h = exp(log_h)
     h_bar = exp(log_h_bar)
     if (i == n_warmup) h = h_bar
-  }
+  }else{noisy_grad=0}
 
-  return(list(pos=x,tau=tau,h=h,w=w,m=m,iter=i+1,Delta=Delta,
-              H_bar = H_bar, h_bar = h_bar))
+  return(list(pos=x,tau=tau,h=h,w=w,m=m,m_adam=m_adam,v_adam=v_adam,iter=i+1,
+              noisy_grad=noisy_grad,Delta=Delta,H_bar = H_bar, h_bar = h_bar))
 }
 
 # hyperparameters for adaptive step size
 h_bar0 = 1
 H_bar0 = 0
 
-adaptive_malt=function(n,init,h0=0.2,n_warmup = 2000, delta = 0.8){
-  update=list(pos=init,tau=1.8,h=h0,w=init,m=init,iter=1,
+#hyperparameters for adaptive integration time
+alpha_adam=0.0005
+beta1_adam=0.9
+beta2_adam=0.999
+eps_adam=10^(-8)
+
+adaptive_malt=function(n,init,h0=0.2,n_warmup = n_warmup, delta = 0.95){
+  update=list(pos=init,tau=h0,h=h0,w=init,m=init,m_adam=0,v_adam=0,iter=1,
               H_bar = H_bar0, h_bar = h_bar0)
   mu = log(10 * h0)
 
@@ -132,18 +153,54 @@ adaptive_malt=function(n,init,h0=0.2,n_warmup = 2000, delta = 0.8){
   h = rep(NA, n)  # stepsize
   alpha = rep(NA, n)  # acceptance prob
   Delta = rep(NA, n)  # change in energy
-
+  tau_values=rep(NA,n)
+  chain=rep(NA,n)
+  noisyg=rep(NA,n)
   for(i in 1:n){
-    update=osam(U,grad,update$pos,update$tau,update$h,update$w,update$m,update$iter,
-                update$H_bar, update$h_bar, mu, n_warmup, delta)
+    update=osam(U,grad,pos=update$pos,tau=update$tau,h=update$h,w=update$w,m=update$m,
+                m_adam=update$m_adam,v_adam=update$v_adam,iter=update$iter,
+                H_bar=update$H_bar, h_bar=update$h_bar, mu=mu, n_warmup=n_warmup, delta=delta)
     eig_values[i]=sqrt(sum(update$w^2))
     means[i]=sum(update$w*update$m)/sqrt(sum(update$w^2))
     h[i] = update$h
     alpha[i] = min(1, exp(-update$Delta))
     Delta[i] = update$Delta
+    tau_values[i]=update$tau
+    chain[i]=update$pos[1]
+    noisyg[i]=sqrt(sum(update$noisy_grad)^2)
   }
 
   return(list(eig_values=eig_values,means=means, h = h, alpha = alpha,
-              Delta = Delta))
+              Delta = Delta,tau_values=tau_values,chain=chain,noisyg=noisyg,
+              last_eigen_vector=update$w))
 }
 
+n=10000
+n_warmup=2000
+output=adaptive_malt(n,init,n_warmup=n_warmup)
+plot(cumsum(output$alpha)/(1:n),type="l",ylim=c(0.75,0.85))
+plot(output$h,type="l",ylim=c(0,0.4))
+plot(output$tau,type="l")
+plot(output$means,type="l")
+plot(output$noisyg,type="l")
+
+
+tau_bar=0
+tau_average=rep(NA,n)
+for(i in 1:n){
+  if(i<=n_warmup){
+  tau_bar=beta2_adam*tau_bar+(1-beta2_adam)*output$tau[i]
+  tau_average[i]=tau_bar/(1-beta2_adam^i)
+  }else{tau_average[i]=tau_average[n_warmup]}
+}
+plot(tau_average,type="l")
+var(output$noisyg)
+var(output$tau)
+tau_average[n]
+
+#install.packages("coda")
+#library(coda)
+effectiveSize(output$chain[n_warmup:n]^2)/tau_average[n]
+
+#plot(output$tau[1:250],type="l")
+#plot(output$tau[1:10],type="l")
